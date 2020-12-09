75b7466234180838d4b5d5f4f5df1d2a6cea6faa
CaliskanBurak <caliskan.burakali@gmail.com>
2020-12-09 00:18:15 -0500

    Add job container

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import useFetchJobs from './useFetchJobs'
-import { Container } from 'react-bootstrap'
+import React, { useState } from "react";
+import useFetchJobs from "./useFetchJobs";
+import { Container } from "react-bootstrap";
+import Job from "./Job";
 
 function App() {
-
-  const { jobs, loading, error } = useFetchJobs()
-
-
+  const [page, setPage] = useState(1);
+  const [params, setParams] = useState({});
+  const { jobs, loading, error } = useFetchJobs(params, page);
 
   return (
     <Container>
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error. Something went wrong</h1>}
-      <h1>{jobs.length}</h1>
+      {jobs.map((job) => {
+        return <Job key={job.id} job={job} />
+      })}
     </Container>
   );
 }
